b007e2900cdd259f993b31e1873c771ab8b797d9
rodrigos89 <rasaraguro@gmail.com>
2017-11-09 22:13:40 -0500

    CITT
    update

--- ejercicioListas.py
+++
@@ -1,7 +0,0 @@
-ventasEmprendimiento = [34, 23.5, 12, 2.4, 12.3]
-
-ventasEmprendimiento+=[23,56,34,56,78]
-ventasEmprendimiento+=[0]*5
-ventasEmprendimiento.extend([23,56,34,56,78])
-print(ventasEmprendimiento)
-
